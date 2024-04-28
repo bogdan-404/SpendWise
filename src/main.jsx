71d4703e4f0e61f320ac33806ce8bc2c2213e9e1
bogdan-404 <bogdan.zlatovcen@gmail.com>
2024-04-28 21:01:25 +0300

    Fix error in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot instead of ReactDOM.render
+
+root.render(
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
+);

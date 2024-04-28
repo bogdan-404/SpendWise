8cd45fdeabd1ae101d62d7932d91c4c9b73c1782
bogdan-404 <bogdan.zlatovcen@gmail.com>
2024-04-28 14:15:02 +0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddExpense from "./pages/AddExpense";
+import EditExpense from "./pages/EditExpense";
+import Statistics from "./pages/Statistics";
 
 function App() {
-
   return (
-  )
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-expense" element={<AddExpense />} />
+          <Route path="/edit-expense/:id" element={<EditExpense />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Routes>
+      </>
+  );
 }
 
-export default App
+export default App;

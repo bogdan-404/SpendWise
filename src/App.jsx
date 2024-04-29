259675e987ea9998e1f3844baa49afe56fe079c1
bogdan-404 <bogdan.zlatovcen@gmail.com>
2024-04-29 11:38:57 +0300

    Add theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import AddExpense from "./pages/AddExpense";
-import EditExpense from "./pages/EditExpense";
-import Statistics from "./pages/Statistics";
-import Navbar from "./components/Navbar";
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import AddExpense from './pages/AddExpense';
+import EditExpense from './pages/EditExpense';
+import Statistics from './pages/Statistics';
+import { ThemeProvider } from './ThemeContext';
 
 function App() {
-  return (
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add-expense" element={<AddExpense />} />
-          <Route path="/edit-expense/:id" element={<EditExpense />} />
-          <Route path="/statistics" element={<Statistics />} />
-        </Routes>
-      </>
-  );
-}
 
+    return (
+        <ThemeProvider>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/add-expense" element={<AddExpense />} />
+                <Route path="/edit-expense/:id" element={<EditExpense />} />
+                <Route path="/statistics" element={<Statistics />} />
+            </Routes>
+        </ThemeProvider>
+    );
+}
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, IconButton, Button } from "@mui/material";
+import React from "react";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
-import DarkModeIcon from '@mui/icons-material/DarkMode'; // Icon for the dark mode switch
-import LightModeIcon from '@mui/icons-material/LightMode'; // Icon for the light mode switch
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { useTheme } from '../ThemeContext';
 
 function Navbar() {
-    const [darkMode, setDarkMode] = useState(false);
-
-    const toggleTheme = () => {
-        setDarkMode(!darkMode);
-        // Here you can also implement the actual theme change logic
-    };
+    const { theme, toggleTheme } = useTheme();
 
     return (
         <AppBar position="static">
@@ -20,11 +16,11 @@ function Navbar() {
                         SpendWise
                     </Link>
                 </Typography>
-                <Button color="inherit" component={Link} to="/statistics">
+                <Link to="/statistics" style={{ textDecoration: 'none', color: 'inherit', marginLeft: 10 }}>
                     Statistics
-                </Button>
+                </Link>
                 <IconButton color="inherit" onClick={toggleTheme}>
-                    {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+                    {theme === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
                 </IconButton>
             </Toolbar>
         </AppBar>

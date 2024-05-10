0437dcc85b8abd4876011fa09ed7ab19360c0d59
bogdan-404 <bogdan.zlatovcen@gmail.com>
2024-05-10 16:02:18 +0300

    add navbar stylization

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,33 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
-import { Link } from "react-router-dom";
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
+// src/components/Navbar.jsx
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Brightness4Icon from '@mui/icons-material/Brightness4'; // Icon for dark theme
+import Brightness7Icon from '@mui/icons-material/Brightness7'; // Icon for light theme
 import { useTheme } from '../ThemeContext';
 
-function Navbar() {
+const Navbar = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={{ backgroundColor: '#192936' }}>
             <Toolbar>
-                <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                    <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <Typography variant="h6" component="div" style={{ flexGrow: 1, fontWeight: 'bold', color: 'white' }}>
+                    <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
                         SpendWise
                     </Link>
                 </Typography>
-                <Link to="/statistics" style={{ textDecoration: 'none', color: 'inherit', marginLeft: 10 }}>
-                    Statistics
-                </Link>
-                <IconButton color="inherit" onClick={toggleTheme}>
-                    {theme === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
+                    <Link to="/statistics" style={{ textDecoration: 'none', color: 'inherit', marginRight: 15 }}>
+                        Statistics
+                    </Link>
+                <IconButton onClick={toggleTheme} color="inherit">
+                    {theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
                 </IconButton>
             </Toolbar>
         </AppBar>
     );
-}
+};
 
 export default Navbar;
+
+
